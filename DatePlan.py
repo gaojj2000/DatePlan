59f7b3353209bf3c2b2a5ef9c3680aba4ffcb114
gaojj2000 <gaojj2000@qq.com>
2022-11-28 10:20:04 +0800

    DatePlanV2版本

--- DatePlan.py
+++ DatePlan.py
@@ -33,7 +33,7 @@ import tkinter.filedialog as file
 
 class DatePlan(tkinter.Tk):
     def __init__(self):
-        super().__init__(className='任务计划器v1')
+        super().__init__(className='任务计划器v2')
         now = time.localtime(time.time())
         self.year = now.tm_year
         self.month = now.tm_mon
@@ -51,6 +51,10 @@ class DatePlan(tkinter.Tk):
         self.weekly = tkinter.IntVar()
         self.start = tkinter.IntVar()
         self.window = tkinter.IntVar()
+        self.set1 = tkinter.IntVar()
+        self.set2 = tkinter.IntVar()
+        self.set3 = tkinter.IntVar()
+        self.set4 = tkinter.IntVar()
         file_name = str(__file__).replace("\\", '/').split("/")
         path = f'C:/Users/{getpass.getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'
         if os.path.isfile(f'{"/".join(file_name[:-1]+["start.bat"])}') and os.path.isfile(f'{path}/s.vbs'):
@@ -59,17 +63,27 @@ class DatePlan(tkinter.Tk):
         self.months_index = -1
         self.days_obj = []
         self.months_obj = []
+        self.events_obj = {}
+        self.index = None
         self.q = False
         self.music = False
         self.player = True
-        self.frame1 = tkinter.Frame(self, height=310, width=250)
+        self.tab = tkinter.ttk.Notebook(self)
+        self.frame3 = tkinter.Frame(self.tab)
+        self.frame4 = tkinter.Frame(self.tab)
+        self.tab.add(self.frame3, text='日期设置')
+        self.tab.add(self.frame4, text='日程管理')
+        self.tab.select(self.frame3)
+        self.tab.pack(expand=1, fill="both")
+
+        self.frame1 = tkinter.Frame(self.frame3, height=310, width=250)
         self.frame1.place(x=0, y=0)
         for r in range(7):
             for c in range(7):
                 button = tkinter.Button(self.frame1, text='', activebackground='skyblue')
                 button.place(x=7.5+c*35, y=7.5+r*35, width=30, height=30)
                 self.days_obj.append(button)
-        self.frame2 = tkinter.Frame(self, height=130, width=210)
+        self.frame2 = tkinter.Frame(self.frame3, height=130, width=210)
         self.frame2.place(x=250, y=0)
         for r in range(3):
             for c in range(4):
@@ -79,42 +93,72 @@ class DatePlan(tkinter.Tk):
         self.move(now.tm_mon)
         self.action(now.tm_mday)
         self.date_calc(self.year, self.month)
-        self.box = tkinter.ttk.Combobox(self, values=[y for y in range(now.tm_year, now.tm_year+101)], state='readonly')
+        self.box = tkinter.ttk.Combobox(self.frame3, values=[y for y in range(now.tm_year, now.tm_year+101)], state='readonly')
         self.box.current(0)
         self.box.place(x=255, y=130, height=20, width=160)
-        self.checkbutton1 = tkinter.Checkbutton(self, text='启用月管理', onvalue=1, offvalue=0, variable=self.mon, command=lambda: self.check('m'))
+        self.checkbutton1 = tkinter.Checkbutton(self.frame3, text='启用月管理', onvalue=1, offvalue=0, variable=self.mon, command=lambda: self.check('m'))
         self.checkbutton1.place(x=250, y=155, height=20, width=80)
-        self.checkbutton2 = tkinter.Checkbutton(self, text='启用日重复', onvalue=1, offvalue=0, variable=self.daily, command=lambda: self.check('d'))
+        self.checkbutton2 = tkinter.Checkbutton(self.frame3, text='启用日重复', onvalue=1, offvalue=0, variable=self.daily, command=lambda: self.check('d'))
         self.checkbutton2.place(x=335, y=155, height=20, width=80)
-        self.checkbutton3 = tkinter.Checkbutton(self, text='启用年管理', onvalue=1, offvalue=0, variable=self.yea, command=lambda: self.check('y'))
+        self.checkbutton3 = tkinter.Checkbutton(self.frame3, text='启用年管理', onvalue=1, offvalue=0, variable=self.yea, command=lambda: self.check('y'))
         self.checkbutton3.place(x=250, y=175, height=20, width=80)
-        self.checkbutton4 = tkinter.Checkbutton(self, text='启用周重复', onvalue=1, offvalue=0, variable=self.weekly, command=lambda: self.check('w'))
+        self.checkbutton4 = tkinter.Checkbutton(self.frame3, text='启用周重复', onvalue=1, offvalue=0, variable=self.weekly, command=lambda: self.check('w'))
         self.checkbutton4.place(x=335, y=175, height=20, width=80)
-        self.checkbutton5 = tkinter.Checkbutton(self, text='复制文件到开机目录', onvalue=1, offvalue=0, variable=self.start, command=lambda: self.check('s'))
+        self.checkbutton5 = tkinter.Checkbutton(self.frame3, text='复制文件到开机目录', onvalue=1, offvalue=0, variable=self.start, command=lambda: self.check('s'))
         self.checkbutton5.place(x=250, y=195, height=20, width=128)
-        self.checkbutton4 = tkinter.Checkbutton(self, text='窗口置顶', onvalue=1, offvalue=0, variable=self.window, command=self.top)
+        self.checkbutton4 = tkinter.Checkbutton(self.frame3, text='窗口置顶', onvalue=1, offvalue=0, variable=self.window, command=self.top)
         self.checkbutton4.place(x=335, y=220, height=25, width=80)
-        self.button1 = tkinter.Button(self, text='回到今日', bg='yellow', command=self.back)
+        self.button1 = tkinter.Button(self.frame3, text='回到今日', bg='yellow', command=self.back)
         self.button1.place(x=255, y=220, height=25, width=80)
-        self.label1 = tkinter.Label(self, text='                  时                  分，消息：', justify=tkinter.LEFT)
+        self.label1 = tkinter.Label(self.frame3, text='                  时                  分，消息：', justify=tkinter.LEFT)
         self.label1.place(x=0, y=260, height=25, width=220)
-        self.entry1 = tkinter.Entry(self)
+        self.entry1 = tkinter.Entry(self.frame3)
         self.entry1.place(x=5, y=260, height=25, width=60)
-        self.entry2 = tkinter.Entry(self)
+        self.entry2 = tkinter.Entry(self.frame3)
         self.entry2.place(x=90, y=260, height=25, width=60)
-        self.entry3 = tkinter.Entry(self)
+        self.entry3 = tkinter.Entry(self.frame3)
         self.entry3.place(x=220, y=260, height=25, width=130)
-        self.button2 = tkinter.Button(self, text='设置', bg='skyblue', command=self.set)
+        self.button2 = tkinter.Button(self.frame3, text='设置', bg='skyblue', command=self.set)
         self.button2.place(x=360, y=260, height=25, width=50)
-        self.label2 = tkinter.Label(self, text='提醒音量(偶数)：', justify=tkinter.LEFT)
+        self.label2 = tkinter.Label(self.frame3, text='提醒音量(偶数)：', justify=tkinter.LEFT)
         self.label2.place(x=0, y=290, height=20, width=100)
-        self.entry4 = tkinter.Entry(self)
+        self.entry4 = tkinter.Entry(self.frame3)
         self.entry4.place(x=100, y=290, height=20, width=40)
-        self.button3 = tkinter.Button(self, text='选择音频文件（英文路径+英文名称）', bg='skyblue', command=self.mp3)
+        self.button3 = tkinter.Button(self.frame3, text='选择音频文件（英文路径+英文名称）', bg='skyblue', command=self.mp3)
         self.button3.place(x=150, y=290, height=20, width=250)
         self.threads(self.target)
         self.box.bind('<<ComboboxSelected>>', self.change)
-        self.geometry('420x320')
+
+        self.button4 = tkinter.Button(self.frame4, text='读取', bg='skyblue', command=self.read)
+        self.button4.place(x=5, y=5, height=25, width=50)
+        self.button5 = tkinter.Button(self.frame4, text='写入', bg='skyblue', command=self.write)
+        self.button5.place(x=65, y=5, height=25, width=50)
+        self.scrollbar = tkinter.Scrollbar(self.frame4)
+        self.scrollbar.place(x=115, y=35, height=270, width=10)
+        self.listbox = tkinter.Listbox(self.frame4, yscrollcommand=self.scrollbar.set)
+        self.listbox.place(x=5, y=35, height=270, width=110)
+        self.scrollbar.config(command=self.listbox.yview)
+        self.listbox.bind('<Double-1>', self.contact_method)
+        self.label3 = tkinter.Label(self.frame4, text="文字消息：\n\n月管理：\n\n年管理：\n\n日重复：\n\n年重复：\n\n音量（偶数）：\n\n音频文件：")
+        self.label3.place(x=130, y=5, height=250, width=80)
+        self.entry5 = tkinter.Entry(self.frame4)
+        self.entry5.place(x=200, y=15, height=20, width=200)
+        self.checkbutton5 = tkinter.Checkbutton(self.frame4, text='启用月管理', onvalue=1, offvalue=0, variable=self.set2, command=lambda: self.check('m'))
+        self.checkbutton5.place(x=260, y=50, height=20, width=80)
+        self.checkbutton6 = tkinter.Checkbutton(self.frame4, text='启用日重复', onvalue=1, offvalue=0, variable=self.set4, command=lambda: self.check('d'))
+        self.checkbutton6.place(x=260, y=85, height=20, width=80)
+        self.checkbutton7 = tkinter.Checkbutton(self.frame4, text='启用年管理', onvalue=1, offvalue=0, variable=self.set1, command=lambda: self.check('y'))
+        self.checkbutton7.place(x=260, y=120, height=20, width=80)
+        self.checkbutton8 = tkinter.Checkbutton(self.frame4, text='启用周重复', onvalue=1, offvalue=0, variable=self.set3, command=lambda: self.check('w'))
+        self.checkbutton8.place(x=260, y=155, height=20, width=80)
+        self.entry6 = tkinter.Entry(self.frame4)
+        self.entry6.place(x=280, y=190, height=20, width=40)
+        self.button6 = tkinter.Button(self.frame4, text='选择音频文件（英文路径+英文名称）', bg='skyblue', command=self.mp33)
+        self.button6.place(x=200, y=225, height=20, width=200)
+        self.button7 = tkinter.Button(self.frame4, text='删除日程', bg='red', command=self.delete_data)
+        self.button7.place(x=160, y=260, height=40, width=200)
+
+        self.geometry('420x340')
         self.resizable(0, 0)
         self.mainloop()
 
@@ -153,21 +197,37 @@ class DatePlan(tkinter.Tk):
                 self.mon.set(0)
                 self.daily.set(0)
                 self.weekly.set(0)
+            if self.set1.get():
+                self.set2.set(0)
+                self.set3.set(0)
+                self.set4.set(0)
         elif t == 'm':
             if self.mon.get():
                 self.yea.set(0)
                 self.daily.set(0)
                 self.weekly.set(0)
-        elif t == 'd':
-            if self.daily.get():
-                self.yea.set(0)
-                self.mon.set(0)
-                self.weekly.set(0)
+            if self.set2.get():
+                self.set1.set(0)
+                self.set3.set(0)
+                self.set4.set(0)
         elif t == 'w':
             if self.weekly.get():
                 self.yea.set(0)
                 self.mon.set(0)
                 self.daily.set(0)
+            if self.set3.get():
+                self.set1.set(0)
+                self.set2.set(0)
+                self.set4.set(0)
+        elif t == 'd':
+            if self.daily.get():
+                self.yea.set(0)
+                self.mon.set(0)
+                self.weekly.set(0)
+            if self.set4.get():
+                self.set1.set(0)
+                self.set2.set(0)
+                self.set3.set(0)
         elif t == 's':
             # 全部启动路径：C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
             # 当前用户启动路径：C:\Users\19305\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
@@ -199,6 +259,20 @@ class DatePlan(tkinter.Tk):
                 #     os.remove(f'{path}/DatePlan.pyw')
                 # if os.path.isfile(f'{path}/task.json'):
                 #     os.remove(f'{path}/task.json')
+
+    def contact_method(self, event):
+        if event:
+            self.index = self.listbox.get(self.listbox.curselection())
+            self.entry5.delete(0, tkinter.END)
+            self.entry5.insert(tkinter.END, self.events_obj[self.index]['task_msg'])
+            self.set1.set(self.events_obj[self.index]['yea_con'])
+            self.set2.set(self.events_obj[self.index]['mon_con'])
+            self.set3.set(self.events_obj[self.index]['weekly'])
+            self.set4.set(self.events_obj[self.index]['daily'])
+            self.entry6.delete(0, tkinter.END)
+            self.entry6.insert(tkinter.END, self.events_obj[self.index]['volume'])
+            if self.events_obj[self.index]['music']:
+                self.button6.configure(text=self.events_obj[self.index]['music'])
 
     def date_calc(self, y: int, m: int):
         now = time.localtime(time.time())
@@ -248,6 +322,18 @@ class DatePlan(tkinter.Tk):
                 else:
                     self.months_obj[4*r+c].configure(bg='white')
 
+    def delete_data(self):
+        if self.index and msg.askyesno(title='注意！', message=f'您正在删除日程 {self.index}，是否继续？'):
+            all_data = eval(open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1] + ['task.json']), 'r', encoding='utf-8').read().replace(' ', ''))
+            add = False
+            for n in range(1, len(all_data)):
+                if n in all_data and (all_data[n]['time'] == self.index or add):
+                    all_data[n] = all_data[n+1]
+                    add = True
+            del all_data[len(all_data)]
+            open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1] + ['task.json']), 'w', encoding='utf-8').write(str(all_data))
+            self.read()
+
     def move(self, m: int):
         # now = time.localtime(time.time())
         self.month = m
@@ -267,6 +353,18 @@ class DatePlan(tkinter.Tk):
             self.music = False
             self.button3.configure(text='选择音频文件（英文路径+英文名称）', bg='skyblue')
 
+    def mp33(self):
+        file_name = file.askopenfilename(filetypes=[('MP3', '*.mp3'), ('WAVE', '*.wav')])
+        if file_name:
+            self.music = file_name
+            self.threads(self.mp3_play, self.music, self.entry6.get() and int(self.entry6.get()) or 20)
+            self.button6.configure(text=self.music, bg='green')
+            if str(msg.showinfo(title='测试音频中...', message='点击 OK 结束测试。')) == 'ok':
+                self.player = False
+        else:
+            self.music = False
+            self.button6.configure(text='选择音频文件（英文路径+英文名称）', bg='skyblue')
+
     def mp3_play(self, music: (str, bool), vol: int = 20):
         if music:
             volume = int(vol / 2)
@@ -278,7 +376,10 @@ class DatePlan(tkinter.Tk):
                 ctypes.windll.user32.PostMessageA(hwnd, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_DOWN * 0x10000)
             for i in range(volume):
                 ctypes.windll.user32.PostMessageA(hwnd, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_UP * 0x10000)
-            self.playsound(music)
+            try:
+                self.playsound(music)
+            except FileNotFoundError:
+                msg.showerror(title='错误！', message=f'音频文件 {music} 损坏或不存在！')
 
     def plan(self, **t):
         while not self.q:
@@ -385,19 +486,12 @@ class DatePlan(tkinter.Tk):
         except OSError:
             msg.showerror(title='ERROR', message='路径或文件名可能含有中文, 或是文件的实际格式不是mp3或mav！')
 
-    @staticmethod
-    def win_command(*command):
-        buf = ctypes.c_buffer(255)
-        command = ' '.join(command).encode(sys.getfilesystemencoding())
-        errorCode = int(ctypes.windll.winmm.mciSendStringA(command, buf, 254, 0))
-        if errorCode:
-            errorBuffer = ctypes.c_buffer(255)
-            ctypes.windll.winmm.mciGetErrorStringA(errorCode, errorBuffer, 254)
-            exceptionMessage = ('\n    Error ' + str(errorCode) + ' for command:'
-                                                                  '\n        ' + command.decode() +
-                                '\n    ' + errorBuffer.value.decode())
-            raise exceptionMessage
-        return buf.value
+    def read(self):
+        self.listbox.delete(0, tkinter.END)
+        json = eval(open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1]+['task.json']), 'r', encoding='utf-8').read())
+        for n in range(1, len(json)+1):
+            self.events_obj[json[n]['time']] = json[n]
+            self.listbox.insert(tkinter.END, json[n]['time'])
 
     def set(self):
         now = time.localtime(time.time())
@@ -411,10 +505,13 @@ class DatePlan(tkinter.Tk):
                     text = f'计划任务来喽！现在的时间是：{self.year}年{self.month}月{self.day}日{self.hour}时{self.minute}分。'
                     if self.entry3.get():
                         text = self.entry3.get()
+                    all_data = eval(open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1]+['task.json']), 'r', encoding='utf-8').read().replace(' ', ''))
+                    for n in range(1, len(all_data)+1):
+                        if all_data[n]['time'] == f'{self.year}-{self.month}-{self.day}-{self.hour}-{self.minute}':
+                            del all_data[n]
                     data = {'time': f'{self.year}-{self.month}-{self.day}-{self.hour}-{self.minute}', 'task_msg': text, 'mon_con': self.mon.get(), 'yea_con': self.yea.get(),
                             'made_time': f'{now.tm_year}-{now.tm_mon}-{now.tm_mday}-{now.tm_hour}-{now.tm_min}', 'daily': self.daily.get(), 'weekly': self.weekly.get(),
                             'music': self.music, 'volume': str(self.entry4.get()).isdigit() and int(self.entry4.get()) or 20}
-                    all_data = eval(open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1]+['task.json']), 'r', encoding='utf-8').read().replace(' ', ''))
                     n = 1
                     while n in all_data:
                         n += 1
@@ -454,6 +551,38 @@ class DatePlan(tkinter.Tk):
             self.wm_attributes('-topmost', 1)
         else:
             self.wm_attributes('-topmost', 0)
+
+    @staticmethod
+    def win_command(*command):
+        buf = ctypes.c_buffer(255)
+        command = ' '.join(command).encode(sys.getfilesystemencoding())
+        errorCode = int(ctypes.windll.winmm.mciSendStringA(command, buf, 254, 0))
+        if errorCode:
+            errorBuffer = ctypes.c_buffer(255)
+            ctypes.windll.winmm.mciGetErrorStringA(errorCode, errorBuffer, 254)
+            exceptionMessage = ('\n    Error ' + str(errorCode) + ' for command:'
+                                                                  '\n        ' + command.decode() +
+                                '\n    ' + errorBuffer.value.decode())
+            raise exceptionMessage
+        return buf.value
+
+    def write(self):
+        if self.index:
+            now = time.localtime(time.time())
+            all_data = eval(open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1] + ['task.json']), 'r', encoding='utf-8').read().replace(' ', ''))
+            text = self.entry5.get() and self.entry5.get() or '计划任务来喽！现在的时间是：{}年{}月{}日{}时{}分。'.format(*self.index.split('-'))
+            for n in range(1, len(all_data)+1):
+                if all_data[n]['time'] == self.index:
+                    all_data[n]['task_msg'] = text
+                    all_data[n]['yea_con'] = self.set1.get()
+                    all_data[n]['mon_con'] = self.set2.get()
+                    all_data[n]['weekly'] = self.set3.get()
+                    all_data[n]['daily'] = self.set4.get()
+                    all_data[n]['music'] = self.music
+                    all_data[n]['volume'] = str(self.entry6.get()).isdigit() and int(self.entry6.get()) or 20
+                    all_data[n]['made_time'] = f'{now.tm_year}-{now.tm_mon}-{now.tm_mday}-{now.tm_hour}-{now.tm_min}'
+            open('/'.join(str(__file__).replace("\\", '/').split('/')[:-1] + ['task.json']), 'w', encoding='utf-8').write(str(all_data))
+            msg.showinfo(title='完成！', message='写入成功！')
 
 
 if __name__ == '__main__':
